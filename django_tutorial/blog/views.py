643442d238f5c49f82d43485b566c22ea38d036e
Zane-dev16 <irellzanedev@gmail.com>
2021-09-15 19:32:44 +0100

    create models for Posts
    Creste models for posts, which with the following info: creator, date publlshed, title and content

--- django_tutorial/blog/views.py
+++ django_tutorial/blog/views.py
@@ -1,27 +1,13 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'IrellZ',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'John Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
     return render(request, 'blog/home.html', context)
 
 
 def about(request):
-    return render(request, 'blog/about.html', {'title': 'About',})
+    return render(request, 'blog/about.html', {'title': 'About', })
